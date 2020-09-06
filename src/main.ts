8d67d8638ce355bbe41232e5afb409e7ce3e230b
Walfrenter <57574880+Walfrenter@users.noreply.github.com>
2020-09-05 21:19:32 -0400

    push initial updates

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,28 @@ const ComponentLibrary = {
   },
 };
 
+const mainFunc = async () => {
+  const url = "https://www.heartofthecards.com/";
+  // fetches html data from iban website
+
+  let res = await fetchData(url);
+
+  if(!res.data){
+    console.log("Invalid data Obj");
+    return;
+  }
+
+  const html = res.data;
+  
+  // mount html page to the root element
+  const $ = cheerio.load(html);
+
+  let dataObj = new Object();
+  // Below will grab the table with the information
+  const allLinks = $('a');
+  
+  const cardSetLinks = allLinks.filter((link) => link.href.includes('cardset='));
+  
+
+}
 export default ComponentLibrary;
