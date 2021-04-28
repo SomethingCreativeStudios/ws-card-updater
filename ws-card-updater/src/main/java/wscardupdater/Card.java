dfd0c9f6155405e671e75679c684679526ce9f44
Walfrenter <57574880+Walfrenter@users.noreply.github.com>
2021-04-27 21:05:02 -0400

    Java Startup
    Initial Setup

---
+++ ws-card-updater/src/main/java/wscardupdater/Card.java
@@ -0,0 +1,12 @@
+package wscardupdater;
+
+public class Card {
+    
+    /*
+    * This Class will handle all the code for creating a new card into
+    * the JSON format with all the fields
+    */
+    public Card(){
+
+    }
+}
