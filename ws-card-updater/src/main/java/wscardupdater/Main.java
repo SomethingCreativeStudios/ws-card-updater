e2f02d034853b0abbf414881f5badd71ab7b1ca3
Walfrenter <57574880+Walfrenter@users.noreply.github.com>
2021-05-08 22:27:03 -0400

    Can get all decks getting cardlist in progress

--- ws-card-updater/src/main/java/wscardupdater/Main.java
+++ ws-card-updater/src/main/java/wscardupdater/Main.java
@@ -1,9 +1,11 @@
 package wscardupdater;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import org.json.simple.*;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.*;
 
 /**
@@ -13,11 +15,17 @@ import org.jsoup.*;
 public class Main 
 {
     
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         
         //System.out.println( "Hello World!" );
-        LinkedList<String> decks = WebScrapper.getListOfDecks();
+        ArrayList<Element> decks = WebScrapper.getListOfDecks();
         
+        for(int i = 0; i < decks.size(); i++ ){
+            
+            ArrayList<Element> cardList = WebScrapper.getListOfCards(decks.get(i));
+            //System.out.println(deckPage);
+            System.exit(0);
+        }
     }
 }
--- ws-card-updater/src/main/java/wscardupdater/WebScrapper.java
+++ ws-card-updater/src/main/java/wscardupdater/WebScrapper.java
@@ -1,7 +1,9 @@
 package wscardupdater;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.*;
 import java.io.IOException;
 
@@ -12,19 +14,66 @@ public class WebScrapper {
 
     
 
-    public static LinkedList<String> getListOfDecks(){
-        LinkedList<String> listOfDecks = null;
+    public static ArrayList<Element> getListOfDecks(){
+        ArrayList<Element> deckListLinks = null;
         Document doc = null;
         try {
             doc = Jsoup.connect(FullDeckListPath).get();
+
+            ArrayList<Element> hrefList = doc.select("a[href]");
+            String filter = "<a href=\"/code/cardlist.html?pagetype=ws&amp;cardset=";
             
-            System.out.println();
+            deckListLinks = findProperLinks(hrefList , filter);
+            System.out.println(deckListLinks.size());
+            
         } catch (IOException e) {
             // Failure of connection to 
             e.printStackTrace();
             System.err.println("Failed to connect to Heart of the Cards Decks List");
         }
-        System.out.println(doc.toString());
-        return listOfDecks;
+        //System.out.println(doc.toString());
+        return deckListLinks;
+    }
+
+    public static ArrayList<Element> findProperLinks(ArrayList<Element> list, String filter){
+        ArrayList<Element> hrefListFinal = new ArrayList();
+        //System.out.println(list.size());
+        
+        for(int i = 0; i < list.size(); i++){
+            Element check = list.get(i);
+            //System.out.println(list.get(i));
+            if(check.toString().contains(filter)){
+                hrefListFinal.add(check);
+            }
+            //if(list.get(i))
+            
+        }
+        System.out.println(hrefListFinal.size());
+        return hrefListFinal;
+    }
+
+    public static ArrayList<Element> getListOfCards(Element deckLink) throws IOException{
+        
+        String link = deckLink.toString();
+        String[] linkSplit = link.split("\"");
+        System.out.println(linkSplit[1]);
+        
+        String finalLink = Constants.HeartoftheCards + linkSplit[1];
+        System.out.println(finalLink);
+        Document deckPage = Jsoup.connect(finalLink).get();
+        
+        ArrayList<Element> hrefList = deckPage.select(".cardlist");
+        ArrayList<Element> cardTable = deckPage.getElementsByClass("cardlist");
+        // NEeds to be fixed can't find table with class=cardlist
+        System.out.println(hrefList);
+        System.exit(0);
+        String filter = "<a href=\"/code/cardlist.html?card=";
+            
+        ArrayList<Element> cardListLinks = findProperLinks(cardTable , filter);
+        for(int i = 0; i < cardListLinks.size(); i++){
+            System.out.println(cardListLinks.get(i));
+        }
+        System.exit(0);
+        return cardListLinks;
     }
 }
